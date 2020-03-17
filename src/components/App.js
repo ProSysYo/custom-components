8fdfa0cd52b8c7a026c03b9ac25d83773c5e8f87
Maxim Ivanov <maksimivanov91@yandex.ru>
2020-03-17 22:59:58 +0300

    Create Menu

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import styles from'./App.module.css';
 import Navbar from './Navbar/Navbar';
+import Menu from './Menu/Menu';
+import Content from './Content/Content';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className={styles.wrap}>
-        <Navbar />
+        <BrowserRouter>     
+            <Navbar />
+            <Menu />
+            <Content /> 
+        </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import styles from './Menu.module.css'
+import { NavLink } from 'react-router-dom'
+
+const MenuItem = (props) => {
+    return (
+        <li className={styles.menuItem}>
+            <NavLink to={props.to} activeClassName={styles.activeLink}>{props.name}</NavLink>            
+        </li>
+    )
+}
+
+const Menu = () => {
+    return (
+        <div className={styles.menu}>
+            <MenuItem to='/input' name='Input'></MenuItem>
+            <MenuItem to='/button' name='Button'></MenuItem>
+            <MenuItem to='/checkbox' name='Checkbox'></MenuItem> 
+            <MenuItem to='/table' name='Table'></MenuItem> 
+            <MenuItem to='/form' name='Form'></MenuItem>
+            <MenuItem to='/card' name='Card'></MenuItem>
+            <MenuItem to='/crud' name='CRUD react+redux'></MenuItem>  
+            <MenuItem to='/other' name='Other'></MenuItem>                
+        </div>
+    )
+}
+
+export default Menu
